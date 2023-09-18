9e7af1006a9cc79f94e13a4700cb6cf8fcdcc6bf
Neto002 <antoniogfneto92@hotmail.com>
2023-09-18 18:03:30 -0300

    alterando nome do site

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="text-center text-lg-start text-muted">
       <div className="text-center p-4 rodape">
-        © 2023 Copyright: LTD - Laboratório de Transformação Digital
+        © 2023 Copyright: Computação / Nutrição
       </div>
     </footer>
   );
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,7 +10,7 @@ export default function Header() {
     >
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
-          LTD-Nutrição
+          Computação / Nutrição
         </a>
         <button
           className="navbar-toggler"
